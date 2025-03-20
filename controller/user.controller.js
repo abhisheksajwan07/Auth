18f59671ba02a804b5c8a1d22e79c9c92ea3bbf4
abhishek sajwan <abhisheksajwan2004@gmail.com>
2025-03-20 23:49:55 +0530

    logout done

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -59,23 +59,32 @@ const verifyUser = async (req, res) => {
 };
 const getMe = async (req, res) => {
   try {
-    const user= await User.findById(req.user.id).select("-password")
-   
+    const user = await User.findById(req.user.id).select("-password");
+
     if (!user) {
-      return res.status(400).json({ success: false, message: "User not found" });
+      return res
+        .status(400)
+        .json({ success: false, message: "User not found" });
     }
-    res.status(200).json({success:false,user:"data"})
+    res.status(200).json({ success: false, user: "data" });
   } catch (error) {
     console.log("Error in getMe:", error.message);
     res.status(500).json({ success: false, message: "Internal Server Error" });
   }
 };
-// const logoutUser = async(req,res){
-//   try{
+const logoutUser = async (req, res) => {
+  try {
+    res.cookie("token", "", {
+      httpOnly: true,
+      secure: true,
+      expires: new Date(0), // Expire immediately
+    });
+    res.status(200).json({ success: true, message: "Logged out successfully" });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
 
-//   }
-//   catch(error){}
-// }
 const login = async (req, res) => {
   const { email, password } = req.body;
   if (!email || !password)
