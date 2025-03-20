848f8d01303a3eb7bc52aa3e34d6fd695988ace2
abhishek sajwan <abhisheksajwan2004@gmail.com>
2025-03-20 23:44:10 +0530

    logout

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -13,7 +13,7 @@ router.post("/register", registerUser);
 router.get("/verify/:token", verifyUser);
 router.post("/login", login);
 router.get("/me", isLoggedIn, getMe);
-router.get
+router.get("/logout",isLoggedIn,logoutUser);
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password/:token", resetPassword);
 export default router;
